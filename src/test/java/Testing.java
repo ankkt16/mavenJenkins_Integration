c3aa169d1f7c0e495055dd5a8ed6fc213ac330f0
Ankit <ankkt16@gmail.com>
2021-02-18 09:41:15 +0530

    Add options for jenkins

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,6 +12,12 @@ class Testing {
     @Test
     public void sampletest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        // options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless"); // !!!should be enabled for Jenkins
+        options.addArguments("--disable-dev-shm-usage"); // !!!should be enabled for Jenkins
+        options.addArguments("--window-size=1920x1080"); // !!!should be enabled for Jenkins
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
